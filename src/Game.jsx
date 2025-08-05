9f1449c57c480ee537c8cc114e4e4cd4d7969721
Zak Chapman <zakchapman@rocketmail.com>
2025-08-05 15:29:07 -0600

    hackathon file update

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,28 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import PokeCard from './PokeCard'
 
 function Game() {
 
   const [ids, setIds] = useState(getTwoRandomIds())
+  const [pokemonSelection, setPokemonSelection] = useState(null)
+  const [poke1Stats, setPoke1Stats] = useState([null])
+  const [poke2Stats, setPoke2Stats] = useState([null])
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${ids[0]}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon/${ids[1]}`).then((res) =>
+        res.json()
+      ),
+    ]).then(([data1, data2]) => {
+      setPoke1Stats(data1);
+      setPoke2Stats(data2);
+      setPokemonSelection(null);
+    });
+  }, [ids]);
+
 
   function getTwoRandomIds(max = 1000) {
     const id1 = Math.floor(Math.random() * max) + 1;
@@ -14,16 +33,23 @@ function Game() {
     }
 
     return [id1, id2];
+
   }
+
+  function handleGuess(index) {
+    setPokemonSelection(index);
+  }
+
+
 
 
 
   return (
     <div>
       <h2>Which Pokemon has higher base stats?</h2>
-      <PokeCard pokeId={ids[0]} />
+      <PokeCard pokeId={ids[0]} stats={poke1Stats} pokeSelected={pokemonSelection === 0} onClick={() => handleGuess(0)} />
       <p>VS</p>
-      <PokeCard pokeId={ids[1]} />
+      <PokeCard pokeId={ids[1]} stats={poke2Stats} pokeSelected={pokemonSelection === 1} onClick={() => handleGuess(1)}/>
       <br />
       <button onClick={() => setIds(getTwoRandomIds())}>New Matchup</button>
     </div>
--- src/PokeCard.jsx
+++ src/PokeCard.jsx
@@ -1,19 +1,20 @@
 import {useState, useEffect} from "react"
+import Game from './Game'
 
-export default function PokeCard({ pokeId }) {
+export default function PokeCard({ pokeId, stats, pokeSelected, onClick }) {
   const [pokeData, setPokeData] = useState(null);
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
       .then(res => res.json())
-      .then(setPokeData)
+      .then(data => setPokeData(data))
   }, [pokeId])
 
   if (!pokeData) return <div>Loading...</div>
 
   return (
-    <div className="pokemon">
-      <h3>{pokeData.name}</h3>
+    <div className="pokemon" onClick={onClick}>
+      <h3>{pokeData.name.toUpperCase()}</h3>
       <img alt={pokeData.name} src={pokeData.sprites.front_default} />
     </div>
   )
